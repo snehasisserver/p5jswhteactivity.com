2d00fca118b17f424130f16aea49468c5e0a5390
Snehasis Ghosh <91667746+snehasisserver@users.noreply.github.com>
2022-06-22 15:52:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload(){
+    
+
+}
+function setup(){
+    canvas = createCanvas(650,500);
+    canvas.position(100,170);
+    video = createCapture(VIDEO);
+video.hide();
+
+   tint_color = "";
+}
+function draw(){
+    image(video,0,0,650,500);
+    tint(tint_color);
+}
+
+function take_snapshot(){
+    save('studentactivityimage.png');
+}
+function filter_tint(){
+    tint_color = document.getElementById("filter_color").value;
+}
